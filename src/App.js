753e093d17da51aacde0410953b1ca43784e27ca
bellh14 <haybell965@gmail.com>
2023-10-27 09:22:41 -0500

    define gloabl colors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./globals.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header Main-bg">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Reviewify!
-        </p>
+        <p>Welcome to Reviewify!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
