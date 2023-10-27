c38726638ad09ddc094c66f5335b77e3caac65eb
bellh14 <haybell965@gmail.com>
2023-10-27 10:12:39 -0500

    update colors to match figma

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import "./globals.css";
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header Main-bg">
+      <header className="App-header Main-bg">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to Reviewify!</p>
         <a
-          className="App-link"
+          className="Link-color"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Create Your Sentiment Score
         </a>
-      </header> */}
+      </header>
     </div>
   );
 }
