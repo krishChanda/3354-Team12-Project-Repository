cbfc905f12249f3c71594bdfc9836943acf63522
bellh14 <haybell965@gmail.com>
2023-10-28 17:59:08 -0500

    update folder structure

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Navbar from "./Components/Navbar";
+import Navbar from "./Components/Shared-Components/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Navbar />
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Navbar />
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
