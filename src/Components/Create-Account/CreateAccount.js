742ebcb333d9842600585c25b27a9e8c28ff1f3a
bellh14 <haybell965@gmail.com>
2023-10-29 12:24:01 -0500

    add icon prop to form component

--- src/Components/Create-Account/CreateAccount.js
+++ src/Components/Create-Account/CreateAccount.js
@@ -1,5 +1,7 @@
 import React from "react";
 import FormTextBox from "../Shared-Components/FormTextBox";
+import email_icon from "../../../public/icons/email_icon.svg";
+import password_icon from "../../../public/icons/password_icon.svg";
 import "./CreateAccount.css";
 
 const CreateAccount = () => {
@@ -8,15 +10,22 @@ const CreateAccount = () => {
             <h2 className="Create-Account-title">
                 <span>Create Account</span>
             </h2>
-            <FormTextBox type="email" name="email" placeholder="EMAIL" />
+            <FormTextBox
+                type="email"
+                name="email"
+                icon={email_icon}
+                placeholder="EMAIL"
+            />
             <FormTextBox
                 type="password"
                 name="password"
+                icon={password_icon}
                 placeholder="PASSWORD"
             />
             <FormTextBox
                 type="password"
                 name="confirm password"
+                icon={password_icon}
                 placeholder="CONFIRM PASSWORD"
             />
         </div>
--- src/Components/Shared-Components/FormTextBox.js
+++ src/Components/Shared-Components/FormTextBox.js
@@ -4,22 +4,29 @@ import "./FormTextBox.css";
 
 const FormTextBox = (FormTextBoxProps) => {
     return (
-        <form>
-            <label>
-                <input
-                    className="FormTextBox"
-                    type={FormTextBoxProps.type}
-                    name={FormTextBoxProps.name}
-                    placeholder={FormTextBoxProps.placeholder}
-                />
-            </label>
-        </form>
+        <div className="FormTextBox-outer">
+            <div className="container">
+                <form className="form">
+                    <div className="FormTextBox-input">
+                        <span class="icon">
+                            <img src={FormTextBoxProps.icon} alt="email icon" />
+                        </span>
+                        <input
+                            type={FormTextBoxProps.type}
+                            name={FormTextBoxProps.name}
+                            placeholder={FormTextBoxProps.placeholder}
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 };
 
 FormTextBox.propTypes = {
     name: PropTypes.string,
     type: PropTypes.string,
+    icon: PropTypes.string,
     placeholder: PropTypes.string,
 };
 
