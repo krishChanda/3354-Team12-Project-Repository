7a3d4f3c6394fd82672424b5344f6a24fcc75224
bellh14 <haybell965@gmail.com>
2023-11-03 16:45:52 -0500

    update index.js to match

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import Login from "./Login/Login";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Login />
+    <Navbar />
+    
   </React.StrictMode>
 );
 
