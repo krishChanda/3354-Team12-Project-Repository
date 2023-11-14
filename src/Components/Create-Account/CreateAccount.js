62a314eb86750f3df33189615b0495f8519231ab
bellh14 <haybell965@gmail.com>
2023-11-14 11:41:22 -0600

    createaccount styling fix not finished

--- src/Components/Create-Account/CreateAccount.js
+++ src/Components/Create-Account/CreateAccount.js
@@ -1,6 +1,6 @@
 // Author: Hayden
 // Test Case 3: The system should register the user account to the system
-// Test Case 4: The system should store the user’s email, first name, and last name in the database 
+// Test Case 4: The system should store the user’s email, first name, and last name in the database
 
 //Create Account page
 
@@ -12,13 +12,13 @@ import password_icon from "../../../public/icons/password_icon.svg";
 import "./CreateAccount.css";
 
 import { useState } from "react"; // useState --> keeps track of user status (logged in or not logged in)
-import {getAuth, createUserWithEmailAndPassword} from "firebase/auth"; // import authentication function
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth"; // import authentication function
 import { useNavigate, Link } from "react-router-dom"; // navigation import
 import { useAddUser } from "../../Hooks/useAddUser"; // adduser function database import
 
 const CreateAccount = () => {
-    //Define AddUser to Database Function 
-    const {addUser} = useAddUser();
+    //Define AddUser to Database Function
+    const { addUser } = useAddUser();
 
     // user data variable declerations
     const [email, setEmail] = useState("");
@@ -27,71 +27,70 @@ const CreateAccount = () => {
     const [firstname, setFirstName] = useState("");
     const [lastname, setLastName] = useState("");
 
-    
-    const auth = getAuth() // Create User into Firebase Database
+    const auth = getAuth(); // Create User into Firebase Database
     const navigate = useNavigate(); // define nagivation function
 
     // when "create account button is clicked"
-    async function handleSignUp(e){
+    async function handleSignUp(e) {
         e.preventDefault();
         // checks if password and confirm password match
-        if (password === confirmPassword){
+        if (password === confirmPassword) {
             // API Call for creating a new user
             createUserWithEmailAndPassword(auth, email, password)
-            .then(async (userCredential) => { // then after creating user --> add user info to database
-                const user = userCredential.user;
-                // gets the current userID
-                if (user) {
-                    const userID = user.uid;
-                    // go to home page
-                    navigate("/home");
-                    // add user Info to database
-                    addUser({ userID, firstname, lastname, email });
-                }  
-                // else there is an authentication error   
-                else {
-                    alert("Error: User not authenticated.");
-                }
-            })
-            // else there is an error in creating the account
-            .catch((error) => {
-                alert(error); // API call handels the error exception in this case
-            });
+                .then(async (userCredential) => {
+                    // then after creating user --> add user info to database
+                    const user = userCredential.user;
+                    // gets the current userID
+                    if (user) {
+                        const userID = user.uid;
+                        // go to home page
+                        navigate("/home");
+                        // add user Info to database
+                        addUser({ userID, firstname, lastname, email });
+                    }
+                    // else there is an authentication error
+                    else {
+                        alert("Error: User not authenticated.");
+                    }
+                })
+                // else there is an error in creating the account
+                .catch((error) => {
+                    alert(error); // API call handels the error exception in this case
+                });
         }
         // else return an error
-        else{
+        else {
             alert("Error, confirm password does not match password");
         }
-        
     }
-    
+
     return (
         <div className="Create-Account">
             <h2 className="Create-Account-title">
                 <span>Create Account</span>
             </h2>
-            <FormTextBox 
+            <FormTextBox
                 onChange={(e) => setEmail(e)}
                 type="email"
                 name="email"
                 icon={email_icon}
                 placeholder="EMAIL"
             />
-            <FormTextBox 
+            <FormTextBox
                 onChange={(e) => setFirstName(e)}
                 type="firstname"
                 name="firstname"
                 icon={email_icon}
                 placeholder="FIRST NAME"
             />
-            <FormTextBox 
+            <FormTextBox
                 onChange={(e) => setLastName(e)}
                 type="lastname"
                 name="lastname"
                 icon={email_icon}
                 placeholder="LASTNAME"
             />
-            <FormTextBox 
+            <FormTextBox
                 onChange={(e) => setPassword(e)}
                 type="password"
                 name="password"
@@ -106,12 +105,20 @@ const CreateAccount = () => {
                 placeholder="CONFIRM PASSWORD"
             />
             <span>
-                <button className="Create-Account-button" onClick={(e) => {handleSignUp(e)}}>
-                    Sign Up 
+                <button
+                    className="Create-Account-button"
+                    onClick={(e) => {
+                        handleSignUp(e);
+                    }}
+                >
+                    Sign Up
                 </button>
             </span>
             <span className="Create-Account-ptext"> Returning User? </span>
-            <span className="Create-Account-signin-text"> <Link to="/"> Login in</Link> </span>
+            <span className="Create-Account-signin-text">
+                {" "}
+                <Link to="/"> Login in</Link>{" "}
+            </span>
         </div>
     );
 };
