34f3aa8db21c0623633e86b8f7744a4c3713f3c6
bellh14 <haybell965@gmail.com>
2023-11-19 23:27:57 -0600

    fix merge conflicts

---
+++ src/Components/Insert-Link/InsertLink.js
@@ -0,0 +1,51 @@
+// Author: Daniel Ng
+// Test Case 13: System should be able to validate different product links
+// Test Case 14: System should allow the user to insert different Amazon product links
+
+// InsertLink.js
+import React from "react";
+import "./InsertLink.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function InsertLink() {
+  const navigate = useNavigate();
+  const [productLink, setProductLink] = useState("");
+
+  const handleHome = () => {
+    navigate('/home');
+  };
+
+  const handleInsertLink = () => {
+    // Check if the productLink contains "www.amazon.com"
+    if (productLink.includes("www.amazon.com")) {
+      // The link contains "www.amazon.com" --> take to link verifed page
+      navigate('/linkverified', { state: { productLink } });
+    } else {
+      // The link doesn't contain "www.amazon.com"
+      alert("Please enter a valid Amazon link.");
+    }
+  }
+
+  return (
+    <div className="app-container">
+      <h1 className="instruction-text">
+        Open the Product Page and Insert Below
+      </h1>
+      <br></br>
+      <input
+        type="text"
+        name="productlink"
+        placeholder="INSERT PRODUCT LINK"
+        onChange={(e) => setProductLink(e.target.value)} 
+      />
+      <br></br>
+      <button className="insert-button" onClick={handleInsertLink}>Insert Link</button>
+      <br></br>
+      <button className="insert-button" onClick={handleHome}> Cancel </button>
+      <br></br>
+    </div>
+  );
+}
+
+export default InsertLink;
